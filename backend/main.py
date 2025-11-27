015099242c2a28d579528acdf454308d4e08b1ba
achlismyusuf <achlismuhammadyusuf18@gmail.com>
2025-11-27 19:50:44 +0700

    update cord middleware config di main.py

--- backend/main.py
+++ backend/main.py
@@ -34,7 +34,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.cors_origins,
+    allow_origins=["*"],  # Atau list of allowed origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
